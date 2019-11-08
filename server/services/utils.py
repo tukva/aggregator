598b98130dc55c2b84e9bb8930e793fa549eb916
tukva <vanton@KH6805.softservecom.com>
2019-11-08 18:27:34 +0200

    Add aggr_by_link_id view

--- server/services/utils.py
+++ server/services/utils.py
@@ -9,3 +9,32 @@ async def match_teams(real_team, all_teams):
             teams_by_link.append(team["name"])
             result[i] = difflib.get_close_matches(real_team, teams_by_link, n=10, cutoff=0.2)
     return result
+
+
+async def get_aggr_teams(request, session):
+    link_id = 1
+    teams = {}
+    while True:
+        async with session.get('http://localhost:8000/parse-links/{link_id}/teams'.format(link_id=link_id)) as resp:
+            if resp.status == 404:
+                break
+            teams[link_id] = await resp.json()
+            link_id += 1
+    if request.raw_args.get("team"):
+        close_matches = await match_teams(request.raw_args["team"], teams)
+        return close_matches
+    async with session.get('http://localhost:8000/real-teams') as resp:
+        teams["real teams"] = await resp.json()
+        return teams
+
+
+async def get_aggr_teams_by_link_id(request, session, link_id):
+    teams = {}
+    async with session.get('http://localhost:8000/parse-links/{link_id}/teams'.format(link_id=link_id)) as resp:
+        teams[link_id] = await resp.json()
+    if request.raw_args.get("team"):
+        close_matches = await match_teams(request.raw_args["team"], teams)
+        return close_matches
+    async with session.get('http://localhost:8000/real-teams') as resp:
+        teams["real teams"] = await resp.json()
+        return teams
--- server/services/views/aggregator.py
+++ server/services/views/aggregator.py
@@ -1,22 +1,16 @@
 import aiohttp
 from sanic.response import json
 
-from services.utils import match_teams
+from services.utils import get_aggr_teams, get_aggr_teams_by_link_id
 
 
-async def aggregator(request):
+async def aggr(request):
     async with aiohttp.ClientSession() as session:
-        link_id = 1
-        teams = {}
-        while True:
-            async with session.get('http://localhost:8000/parse-links/{link_id}/teams'.format(link_id=link_id)) as resp:
-                if resp.status == 404:
-                    break
-                teams[link_id] = await resp.json()
-                link_id += 1
-        if request.raw_args.get("team"):
-            real_team = await match_teams(request.raw_args["team"], teams)
-            return json(real_team)
-        async with session.get('http://localhost:8000/real-teams') as resp:
-            teams["real teams"] = await resp.json()
-            return json(teams)
+        result = await get_aggr_teams(request, session)
+        return json(result)
+
+
+async def aggr_by_link_id(request, link_id):
+    async with aiohttp.ClientSession() as session:
+        result = await get_aggr_teams_by_link_id(request, session, link_id)
+        return json(result)
