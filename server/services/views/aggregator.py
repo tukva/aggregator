7795b67c1bd796b71dcd6a743235b882d608d1a3
tukva <vanton@KH6805.softservecom.com>
2019-11-13 17:12:44 +0200

    Add unit tests and parser client from common package

--- server/services/views/aggregator.py
+++ server/services/views/aggregator.py
@@ -5,12 +5,11 @@ from services.utils import get_aggr_teams, get_aggr_teams_by_link_id
 
 
 async def aggr(request):
-    async with aiohttp.ClientSession() as session:
-        result = await get_aggr_teams(request, session)
-        return json(result)
+    result = await get_aggr_teams(request)
+    return json(result, 200)
 
 
 async def aggr_by_link_id(request, link_id):
     async with aiohttp.ClientSession() as session:
         result = await get_aggr_teams_by_link_id(request, session, link_id)
-        return json(result)
+        return json(result, 200)
