6561d725834ff92e47e04f7bf4ef6791104a6431
tukva <vanton@KH6805.softservecom.com>
2019-11-22 11:46:49 +0200

    Fix config

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
 from sanic import Sanic
 
-from config import AGGREGATOR_API_POST
+from config import AGGREGATOR_API_PORT
 from routes import add_routes
 
 app = Sanic(name=__name__)
 add_routes(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=AGGREGATOR_API_POST)
+    app.run(host='0.0.0.0', port=AGGREGATOR_API_PORT)
