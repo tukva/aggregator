272c079a6b822aa5dbdb627683d9d902e0494da4
tukva <vanton@KH6805.softservecom.com>
2019-12-11 13:32:03 +0200

    Http status from const

--- server/services/views/aggregator.py
+++ server/services/views/aggregator.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from sanic.response import json
 
 from services.utils import get_aggr_teams
@@ -5,9 +7,9 @@ from services.utils import get_aggr_teams
 
 async def aggr(request):
     result = await get_aggr_teams(request)
-    return json(result, 200)
+    return json(result, HTTPStatus.OK)
 
 
 async def aggr_by_link_id(request, link_id):
     result = await get_aggr_teams(request, link_id)
-    return json(result, 200)
+    return json(result, HTTPStatus.OK)
