82819ae17f160d60b422ba9dedc43b2f7e1cef69
tukva <vanton@KH6805.softservecom.com>
2019-12-12 18:24:52 +0200

    Add http const to unit tests

--- server/tests/test_aggregator.py
+++ server/tests/test_aggregator.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from unittest import mock
 
 import pytest
@@ -10,14 +11,14 @@ async def test_aggregator(test_cli, mock_resp_teams, mock_resp_real_teams):
         with mock.patch.object(BaseClientBettingData, 'get_teams', return_value=mock_resp_teams):
             resp = await test_cli.get('/aggregate')
 
-            assert resp.status == 200
+            assert resp.status == HTTPStatus.OK
             assert await resp.json() == {'1': [{'name': 'chelsea', 'link_id': 1},
                                                {'name': 'manchester united', 'link_id': 1}],
                                          'real_teams': [{'name': 'FC Chelsea'}, {'name': 'Liverpool'}]}
 
             resp = await test_cli.get('/aggregate?team=FC Chelsea')
 
-            assert resp.status == 200
+            assert resp.status == HTTPStatus.OK
             assert await resp.json() == {'1': ['chelsea', 'manchester united'],
                                          'real_teams': 'FC Chelsea'}
 
@@ -28,13 +29,13 @@ async def test_aggregator_by_link(test_cli, mock_resp_teams, mock_resp_real_team
         with mock.patch.object(BaseClientBettingData, 'get_real_teams', return_value=mock_resp_real_teams):
             resp = await test_cli.get('/aggregate/1')
 
-            assert resp.status == 200
+            assert resp.status == HTTPStatus.OK
             assert await resp.json() == {'1': [{'name': 'chelsea', 'link_id': 1},
                                                {'name': 'manchester united', 'link_id': 1}],
                                          'real_teams': [{'name': 'FC Chelsea'}, {'name': 'Liverpool'}]}
 
             resp = await test_cli.get('/aggregate/1/?team=FC Chelsea')
 
-            assert resp.status == 200
+            assert resp.status == HTTPStatus.OK
             assert await resp.json() == {'1': ['chelsea', 'manchester united'],
                                          'real_teams': 'FC Chelsea'}
